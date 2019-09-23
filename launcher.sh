a594bdb11a21fff7f176ffbc00f3bcd63b24fdd8
Runar Myklebust <rmy@enonic.com>
2019-09-23 16:18:35 +0200

    Update launcher.sh
    Added option to avoid random sleep, also switched to apache for certbot initial generation of certs

--- launcher.sh
+++ launcher.sh
@@ -7,10 +7,10 @@ then
     DOMAIN_CMD="-d $(echo $LETS_ENCRYPT_DOMAINS | sed 's/,/ -d /')"
   fi
 
-  certbot -n certonly --no-self-upgrade --agree-tos --standalone -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) $DOMAIN_CMD
+  certbot -n certonly --no-self-upgrade --agree-tos --apache -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) $DOMAIN_CMD
   ln -s /etc/letsencrypt/live/$(hostname -f) /etc/letsencrypt/certs
 else
-  certbot renew --no-self-upgrade
+  certbot renew --no-random-sleep-on-renew --no-self-upgrade
 fi
 
 echo "Launching apache2."
